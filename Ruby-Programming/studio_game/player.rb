223d1791a718db2818ec6cec1078557ba70d9f66
JoshHarris85 <joshua.harris1085@gmail.com>
2019-11-20 22:37:03 -0600

    Finish CH 11

---
+++ Ruby-Programming/studio_game/player.rb
@@ -0,0 +1,41 @@
+class Player
+  attr_reader :health
+  attr_reader :name
+
+  def initialize(name, health = 100)
+    @name = name.capitalize
+    @health = health
+  end
+
+  def name=(new_name)
+    @name = new_name.capitalize
+  end
+
+  def to_s
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
+  end
+
+  def blam
+    @health -= 10
+    puts "#{@name} got blammed!"
+  end
+
+  def woot
+    @health += 15
+    puts "#{@name} got w00ted!"
+  end
+
+  def score
+    @health + @name.length
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  player = Player.new('moe')
+  puts player.name
+  puts player.health
+  player.w00t
+  puts player.health
+  player.blam
+  puts player.health
+end
---
+++ Ruby-Programming/studio_game/studio_game.rb
@@ -0,0 +1,22 @@
+require_relative 'player'
+require_relative 'game'
+
+player1 = Player.new('moe')
+player2 = Player.new('larry', 60)
+player3 = Player.new('curly', 125)
+
+knuckleheads = Game.new('Knuckleheads')
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
+
+chipmunk1 = Player.new('alvin')
+chipmunk2 = Player.new('Simon', 60)
+chipmunk3 = Player.new('theodore', 125)
+
+chipmunks = Game.new('Chipmunks')
+chipmunks.add_player(chipmunk1)
+chipmunks.add_player(chipmunk2)
+chipmunks.add_player(chipmunk3)
+chipmunks.play
