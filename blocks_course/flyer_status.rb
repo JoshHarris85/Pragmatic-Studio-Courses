d311eaad8064a00033ba5067f50cec8d7853052f
JoshHarris85 <joshua.harris1085@gmail.com>
2019-12-29 03:29:53 -0600

    CH 3 PT 2 - Block Basics

--- blocks_course/flyer_status.rb
+++ blocks_course/flyer_status.rb
@@ -29,3 +29,28 @@ puts non_frequent_flyers
 puts flyers.any? { |f| f.status == :platinum }
 
 puts flyers.find { |f| f.status == :bronze }
+
+platinum_flyers, regular_flyers = flyers.partition { |f| f.status == :platinum }
+
+puts "platinum flyers:"
+puts platinum_flyers
+
+puts "regular flyers:"
+puts regular_flyers
+
+puts flyers.map { |f| "#{f.name} (#{f.status.upcase})" }
+
+puts "flown in km:"
+km_miles = flyers.map { |f| f.miles_flown * 1.6 }
+puts km_miles
+
+sum_miles = flyers.reduce(0) { |sum, f| sum + f.miles_flown }
+puts sum_miles
+
+sum_km = km_miles.reduce(0, :+)
+puts sum_km
+
+puts flyers.select { |f| f.status == :bronze }
+           .reduce(0) { |sum, f| sum + (f.miles_flown * 1.6) }
+
+puts flyers.max_by { |f| f.miles_flown }
--- blocks_course/scores.rb
+++ blocks_course/scores.rb
@@ -12,6 +12,19 @@ puts scores.any? { |s| s < 70 }
 
 puts scores.find { |s| s < 70 }
 
-scores.select! { |s| s > 70 }
-scores.reject! { |s| s.even? }
-puts scores
+# scores.select! { |s| s > 70 }
+# scores.reject! { |s| s.even? }
+# puts scores
+
+scores_doubled = scores.map { |s| s * 2 }
+puts "Double scores:"
+puts scores_doubled
+
+puts scores.reduce(0, :+) > 600
+
+evens, odds = scores.partition { |s| s.even? }
+puts "evens:"
+puts evens
+
+puts "odds:"
+puts odds
