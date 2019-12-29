4e7596b21324367b6277ba2978bad04dbac251ff
JoshHarris85 <joshua.harris1085@gmail.com>
2019-12-28 18:31:14 -0600

    Finish CH 2 block basics

---
+++ blocks_course/block_basics.rb
@@ -0,0 +1,12 @@
+1.upto(5) do |n|
+  puts "#{n} situp"
+  puts "#{n} pushup"
+  puts "#{n} chinup"
+end
+
+
+1.step(9, 2) do |n|
+  puts "#{n} situp"
+  puts "#{n} pushup"
+  puts "#{n} chinup"
+end
